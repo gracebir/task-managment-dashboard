398049715b6f705d0125a15df5179eb668fe3bfd
gracebir <grace.devolop@gmail.com>
2024-10-23 08:20:17 +0200

    setting up rtk

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,3 +1,17 @@
 /** @format */
 
 import { configureStore } from "@reduxjs/toolkit";
+import { baseApiSlice } from "./slices/baseApiSlice";
+
+export const store = configureStore({
+    reducer: {
+        [baseApiSlice.reducerPath]: baseApiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(baseApiSlice.middleware),
+    devTools: true,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/** @format */
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./lib/store.ts";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </StrictMode>
+);
