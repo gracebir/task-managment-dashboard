8243cd469cc5d439cdaeeda6dff105e818ffc7a0
gracebir <grace.devolop@gmail.com>
2024-10-23 08:19:19 +0200

    base slice api

---
+++ src/lib/slices/baseApiSlice.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApiSlice = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "'https://dummyjson.com'",
+    }),
+    tagTypes: ["todos"],
+    endpoints: () => ({}),
+});
