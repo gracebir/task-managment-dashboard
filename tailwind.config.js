95d95b303aea3e15d81c049f45e2bce142723566
gracebir <grace.devolop@gmail.com>
2024-10-24 18:44:40 +0200

    tailwindcss colors value

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ export default {
             fontFamily: {
                 montserrat: ["Montserrat", "sans-serif"],
             },
+            colors: {
+                primary: "#F6F8FA",
+                "primary-dark": "#0d1824",
+                secondary: "#041324",
+                pink: "#5653C3",
+                "light-pink": "#F4F2FB",
+                "light-progress": "#EDF9F5",
+                progress: "#78B9E7",
+                success: "#71B297",
+                "light-success": "#F2F7FB",
+                todo: "#E2B88B",
+                "light-todo": "#F9F4ED",
+                online: "#AFEAA1",
+            },
         },
         screen: {
             ...defaultTheme,
